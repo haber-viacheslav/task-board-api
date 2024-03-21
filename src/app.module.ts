880c1affedce5011caacf5586ef556e968c15ae4
Haber Viacheslav <gaberslavik@gmail.com>
2024-03-21 16:05:44 +0200

    added db

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "password",
+      database: "task-board",
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
